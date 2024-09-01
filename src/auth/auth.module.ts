4843498af2d7577250056fd7207d5782d9cc10aa
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-09-01 22:41:14 +0200

    6. Mise en œuvre du JWT Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
   imports: [
     UsersModule,
     PassportModule,
@@ -16,6 +16,7 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
